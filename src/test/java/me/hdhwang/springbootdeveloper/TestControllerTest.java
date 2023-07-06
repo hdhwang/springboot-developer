bfefad86b824b5e876abcbca97b56328eed62547
hdhwang <hadong0624@naver.com>
2023-07-06 21:43:12 +0900

    First Commit

---
+++ src/test/java/me/hdhwang/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,34 @@
+package me.hdhwang.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest //테스트용 애플리케이션 컨텍스트 생성
+@AutoConfigureMockMvc // MockMvc 생성
+class TestControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+}
